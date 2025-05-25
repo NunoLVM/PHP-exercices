9b3184e96efe096d5fb9955de96b49a07ab8fbb4
nuno <nunopunkas@gmail.com>
2025-05-25 23:51:27 +0200

    first commit

---
+++ exercice.php
@@ -0,0 +1,24 @@
+<?php
+    $ville = "Paris";
+    echo $ville;
+
+?>
+
+<?php
+    $prenom = "Jean";
+    $nom = "Dupont";
+
+    echo $prenom . $nom;
+
+$>
+
+<?php 
+    $score = 10; 
+    echo $score; 
+    
+    $score = 15; 
+    echo $score;
+?>
+
+
+
