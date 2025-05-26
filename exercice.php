2cf05ffb2613fddb14be01b5b850d8dc818418ed
nuno <nunopunkas@gmail.com>
2025-05-26 11:17:28 +0200

    fix php | exercices added

--- exercice.php
+++ exercice.php
@@ -1,24 +1,56 @@
 <?php
     $ville = "Paris";
-    echo $ville;
+    echo "<p>" . strtoupper($ville) . "</p>";
 
 ?>
 
-<?php
+<p><?php
     $prenom = "Jean";
     $nom = "Dupont";
 
-    echo $prenom . $nom;
+    echo $prenom . " " . $nom;
 
-$>
+?></p>
 
 <?php 
     $score = 10; 
-    echo $score; 
+    echo "<p>" . $score . "</p>"; 
     
     $score = 15; 
-    echo $score;
+    echo "<p>" . $score . "</p>";
 ?>
 
+<p>
+<?php
+    $a = 8;
+    $b = 4;
+    $total = $a + $b;
+    echo "Le total est: " . $total;
+?>
+</p>
 
+<p>
+<?php
+    $texte = "alo alo";
+    $entier = 10;
+    $decimal = 0.33;
+    $vraiOuFaux = true;
 
+    var_dump($texte);
+    echo "<br>";
+    var_dump($entier);
+    echo "<br>";
+    var_dump($decimal);
+    echo "<br>";
+    var_dump($vraiOuFaux);
+?>
+</p>   
+
+<?php
+   $note_maths = 15;
+   $note_francais = 12;
+   $note_histoire_geo = 9;
+   $moyenne = 0;
+   echo 'La moyenne est de '.$moyenne.' / 20.';
+?>
+  
---
+++ index.php
@@ -0,0 +1,13 @@
+<html lang="en">
+<head>
+
+    <meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+    <title>Php exercices</title>
+</head>
+<body>
+
+    <?php include('exercice.php'); ?>
+</body>
+
+</html>
